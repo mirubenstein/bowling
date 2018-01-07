448b0c76ff7966cf94b10acec5ee12c8a6d2189d
Mark R <mirubenstein@gmail.com>
2018-01-06 21:31:40 -0800

    Add base solution. Needs serious clean up WIP. Also getting warning for toplevel constant

---
+++ bowling.rb
@@ -0,0 +1,105 @@
+class BowlingError < StandardError
+end
+
+class Frame
+  attr_reader :pins
+
+  def initialize
+    @pins = []
+  end
+
+  def add_pins(pin)
+    pins << pin
+    raise BowlingError unless valid?
+  end
+
+  def done?
+    strike? || pins.count == 2
+  end
+
+  def score(next_frames)
+    case
+      when strike?
+        raw_score + next_frames.flat_map(&:pins).first(2).sum
+      when spare?
+        raw_score + next_frames.flat_map(&:pins).first
+      else
+        raw_score
+    end
+  end
+
+  private
+
+  def spare?
+    raw_score == 10 && pins.count == 2
+  end
+
+  def strike?
+    pins.first == 10
+  end
+
+  def raw_score
+    pins.sum
+  end
+
+  def valid?
+    raw_score.between?(0, 10)
+  end
+end
+
+
+class LastFrame < Frame
+  def done?
+    pins.count == 3 || (pins.count == 2 && raw_score < 10)
+  end
+
+  def valid?
+    pins.all? { |p| p.between?(0, 10) } &&
+      raw_score.between?(0, 30) &&
+      strike_valid?
+  end
+
+  def strike_valid?
+    case
+      when pins.first(2).sum == 20
+        true
+      when pins.first == 10
+        raw_score <= 20
+      else
+        true
+    end
+  end
+end
+
+class Game
+  attr_reader :frames
+
+  def initialize
+    @frames = []
+    @current_frame = Frame.new
+  end
+
+  def score
+    raise BowlingError unless frames.count == 10
+    frames.each.with_index(1).sum do |frame, index|
+      frame.score(frames[index, index + 1] || [])
+    end
+  end
+
+  def roll(pins)
+    raise BowlingError if frames.count == 10
+    @current_frame.add_pins pins
+    if @current_frame.done?
+      frames << @current_frame
+      @current_frame = if frames.count == 9
+        LastFrame.new
+      else
+        Frame.new
+      end
+    end
+  end
+end
+
+module BookKeeping
+  VERSION = 3
+end
